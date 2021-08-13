5a85a3b3a94f94b670a9329ddcfad73c8929b3ab
Rashed-swe <86867835+Rashed-swe@users.noreply.github.com>
2021-08-13 16:04:50 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+"use strict";
+function video(){
+	document.write(`
+	<body style="background:;">
+	<video style="width: 100%; height: 100vh" src="video/video.mkv" autoplay muted>
+		
+		</video>
+		<a href="index.html" style=" position: absolute; top: 15px; right: 20px" title="Close"><img src="images/close.png" alt="close" style="width: 40px"></a>
+		</body>
+	`);
+};
